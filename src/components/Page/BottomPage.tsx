1629a4348ee050a252c3d4737a5133bcb6d5ce5e
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-12-17 19:50:30 +0900

    一旦fix

--- src/components/Page/BottomPage.tsx
+++ src/components/Page/BottomPage.tsx
@@ -1,5 +1,11 @@
 const BottomPage = () => {
-  return <div className="page4 h-screen "></div>;
+  return (
+    <div className="page4 h-screen relative">
+      <button className="absolute bottom-1/4 left-1/2 transform -translate-x-1/2 bg-red-200 p-4 rounded-full w-auto font-bold">
+        more details
+      </button>
+    </div>
+  );
 };
 
 export default BottomPage;
